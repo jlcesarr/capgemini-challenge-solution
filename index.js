4c72c4fe7a13c8da20994e816f77203b1712e91b
Júlio César <74342193+jlcesarr@users.noreply.github.com>
2022-02-15 22:41:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
 
-// comente a linha abaixo para desabilitar a interação via terminal
+import { ladder } from './modules/ladder.js'
+import { password } from './modules/password.js'
+import { anagram } from './modules/anagram.js'
+
+
+// comente a linha abaixo para desabilitar a interação via terminal (prompt)
 import "./prompt.js";
+
+
+// utilizando as funcionalidades - exemplos das questões
+// ladder.buildLadder(6) // questão 1
+// password.validate("Ya3") // questão 2
+// anagram.getAnagrams("ovo") // questão 3
